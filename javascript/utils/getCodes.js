83ff7a1bc56a1e0e8690711741dd172b185142ed
euberdeveloper <euberdeveloper@gmail.com>
2019-11-22 14:17:32 +0100

    removed everything

--- javascript/utils/getCodes.js
+++
@@ -1,9 +0,0 @@
-function getCodes(generators, structure, messages) {
-    return Object.keys(generators)
-        .map(key => ({
-            comment: generators[key].comment,
-            code: (new generators[key].generator(structure, messages)).generate()
-        }));
-}
-
-module.exports = getCodes;
--- javascript/utils/index.js
+++
@@ -1,9 +0,0 @@
-const parseTemplate = require('./parseTemplate');
-const getCodes = require('./getCodes');
-const MessagesJsonGenerator = require('./messagesJsonGenerator');
-
-module.exports = {
-    parseTemplate,
-    getCodes,
-    MessagesJsonGenerator
-};
